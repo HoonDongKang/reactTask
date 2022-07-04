e7233a9efd2d4fec8f578f1c727276e5ff2ed58b
Donghoon Kang <d159123@naver.com>
2022-07-04 17:55:24 +0900

    frontEnd

--- src/components/Signup.style.tsx
+++ src/components/Signup.style.tsx
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 export const Formcontainer = styled.form`
     @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
     font-family: 'Roboto Slab', serif;
-    margin: 100px 20px;
+    margin: 40px 40px;
     border: 1px solid black;
     border-radius: 10px;
-    height: 700px;
+    height: 1000px;
+    width: 400px;
     display: flex;
     flex-direction: column;
     > div {
@@ -14,18 +15,91 @@ export const Formcontainer = styled.form`
         display: flex;
         flex-direction: column;
         > p {
-            margin: 50px 30px;
+            margin: 50px 30px 0px 30px;
             font-weight: 700;
-            font-size: xx-large;
+            font-size: 25px;
+            > .aTag {
+                font-size: 30px;
+                color: #0e73cc;
+                text-decoration: none;
+            }
+        }
+        > span {
+            font-size: 15px;
+            color: #616161;
+            margin: 20px 30px;
         }
         > label {
             margin: 10px 20px;
-            font-size: x-large;
+            font-size: 18px;
             display: block;
             flex: 1;
         }
         > input {
-            flex: 1;
+            height: 30px;
+            margin: 10px 30px 50px 30px;
+            width: 300px;
+            border: 1px solid #c1c1c1;
+            background-color: #fafafa;
+            border-radius: 3px;
+        }
+        > input[type='number']::-webkit-outer-spin-button,
+        > input[type='number']::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+        > input:hover {
+            border-bottom: 2px solid #a8a8a8;
+        }
+        > select {
+            width: 300px;
+            height: 30px;
+            margin: 10px 30px 50px 30px;
+            background-color: #fafafa;
+            border: 1px solid #c1c1c1;
+        }
+        > select:hover {
+            background-color: #e7e5e5ec;
+        }
+    }
+    > .policyTag {
+        margin-left: 30px;
+        font-size: 15px;
+        > input {
+            margin-right: 14px;
+        }
+    }
+    > .signUpTag {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        > input {
+            margin: 20px auto;
+            width: 300px;
+            height: 30px;
+            background-color: #2b2b2b;
+            color: #fafafa;
+            border-radius: 9px;
+        }
+        > input:hover {
+            background-color: #161616;
+            cursor: pointer;
+        }
+        > button {
+            display: flex;
+            width: 300px;
+            align-items: center;
+            justify-content: center;
+            margin: 30px auto;
+            font-weight: 700;
+            background-color: #fafafa;
+            border-radius: 9px;
+            border: 1px solid #c1c1c1;
+            height: 30px;
+        }
+        > button:hover {
+            background-color: #e7e5e5ec;
+            cursor: pointer;
         }
     }
 `
@@ -34,11 +108,17 @@ export const PageDivcontainer = styled.div`
     display: flex;
     flex-direction: row;
     > .imgDiv {
-        background-color: blue;
         flex: 2;
+        border-right: 1px solid black;
+        > img {
+            width: 400px;
+            height: 600px;
+            object-fit: cover;
+            margin: 200px;
+            margin-left: 300px;
+        }
     }
     > .signUpDiv {
         flex: 1;
-        background-color: red;
     }
 `
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,7 +1,8 @@
 import useInput from '../hooks/useInput'
 import { Formcontainer, PageDivcontainer } from './Signup.style'
 import { toast } from 'react-toastify'
-
+import Link from 'next/link'
+import dog from '../img/dog.jpg'
 
 const Signup = () => {
     //useInput module
@@ -26,58 +27,149 @@ const Signup = () => {
             toast.error('Please fill all blanks')
         }
     }
+    const nationArr = [
+        'Algeria',
+        'Angola',
+        'Austria',
+        'Australia ',
+        'Bermuda ',
+        'Bosnia ',
+        'China ',
+        'Cook ',
+        'Czech Republic',
+        'Dominica ',
+        'France ',
+        'Hong Kong',
+        'Japan ',
+        'Korea North',
+        'Korea South',
+        'New Zealand',
+    ]
+
+    // const inputArr = [
+    //     { name: 'ID', type: 'text', value: input.id },
+    //     {
+    //         name: 'Password',
+    //         type: 'password',
+    //         value: input.pw,
+    //     },
+    //     { name: 'Name', type: 'text', value: input.name },
+    //     { name: 'Age', type: 'number', value: input.age },
+    // ]
+
     return (
         <>
             <PageDivcontainer>
-                <div className='imgDiv'></div>
+                <div className='imgDiv'>
+                    <img src='dog.jpg' alt='dog'></img>
+                </div>
                 <div className='signUpDiv'>
                     <Formcontainer>
                         <div>
                             <p>Sign Up</p>
+                            <span>
+                                Already have an accout?{' '}
+                                <Link className='aTag' href='/'>
+                                    Sign in
+                                </Link>
+                            </span>
                         </div>
+                        {/* map을 통해서 사용하면 onChange가 작동하지 않음,,("?")
+                        {inputArr.map((inputObj) => {
+                            return (
+                                <div
+                                    key={inputObj.name}
+                                    style={{ margin: '10px' }}
+                                >
+                                    <label htmlFor={inputObj.name}>
+                                        {inputObj.name}
+                                    </label>
+                                    <input
+                                        type={inputObj.type}
+                                        name={inputObj.name}
+                                        value={inputObj.value}
+                                        onChange={setInputs}
+                                    ></input>
+                                </div>
+                            )
+                        })} */}
                         <div>
-                            <label>ID:</label>
+                            <label>ID</label>
                             <input
                                 name='id'
                                 type='text'
-                                placeholder='ID'
                                 value={input.id}
                                 onChange={setInputs}
                             ></input>
                         </div>
                         <div>
-                            <label>Password:</label>
+                            <label>Password</label>
                             <input
                                 name='pw'
                                 type='password'
-                                placeholder='PW'
                                 value={input.pw}
                                 onChange={setInputs}
                             ></input>
                         </div>
                         <div>
-                            <label>Name:</label>
+                            <label>Name</label>
                             <input
                                 name='name'
                                 type='text'
-                                placeholder='Name'
                                 value={input.name}
                                 onChange={setInputs}
                             ></input>
                         </div>
                         <div>
-                            <label>Age:</label>
+                            <label>Age</label>
                             <input
                                 name='age'
                                 type='number'
-                                placeholder='Age'
                                 value={input.age}
                                 onChange={setInputs}
                             ></input>
                         </div>
                         <div>
-                            <input type='submit' onClick={submit}></input>
+                            <label htmlFor='country'>Country</label>
+                            <select id='country'>
+                                Country
+                                {nationArr.map((nation) => {
+                                    return (
+                                        <option value={nation} key={nation}>
+                                            {nation}
+                                        </option>
+                                    )
+                                })}
+                            </select>
                         </div>
+                        <span className='policyTag'>
+                            <input type='checkbox' id='agree'></input>
+                            <label htmlFor='agree'>
+                                I have read and agree to the Terms of Service
+                            </label>
+                        </span>
+                        <span className='signUpTag'>
+                            <input
+                                type='submit'
+                                onClick={submit}
+                                value='Sign up'
+                            ></input>
+                            <button>
+                                <svg
+                                    width='32'
+                                    height='20px'
+                                    viewBox='0 0 32 32'
+                                    fill='none'
+                                    xmlns='http://www.w3.org/2000/svg'
+                                >
+                                    <path
+                                        d='M16 0C7.16 0 0 7.16 0 16C0 23.08 4.58 29.06 10.94 31.18C11.74 31.32 12.04 30.84 12.04 30.42C12.04 30.04 12.02 28.78 12.02 27.44C8 28.18 6.96 26.46 6.64 25.56C6.46 25.1 5.68 23.68 5 23.3C4.44 23 3.64 22.26 4.98 22.24C6.24 22.22 7.14 23.4 7.44 23.88C8.88 26.3 11.18 25.62 12.1 25.2C12.24 24.16 12.66 23.46 13.12 23.06C9.56 22.66 5.84 21.28 5.84 15.16C5.84 13.42 6.46 11.98 7.48 10.86C7.32 10.46 6.76 8.82 7.64 6.62C7.64 6.62 8.98 6.2 12.04 8.26C13.32 7.9 14.68 7.72 16.04 7.72C17.4 7.72 18.76 7.9 20.04 8.26C23.1 6.18 24.44 6.62 24.44 6.62C25.32 8.82 24.76 10.46 24.6 10.86C25.62 11.98 26.24 13.4 26.24 15.16C26.24 21.3 22.5 22.66 18.94 23.06C19.52 23.56 20.02 24.52 20.02 26.02C20.02 28.16 20 29.88 20 30.42C20 30.84 20.3 31.34 21.1 31.18C24.2763 30.1077 27.0363 28.0664 28.9917 25.3432C30.947 22.6201 31.9991 19.3524 32 16C32 7.16 24.84 0 16 0Z'
+                                        fill='currentColor'
+                                    ></path>
+                                </svg>
+                                Continue with GitHub
+                            </button>
+                        </span>
                     </Formcontainer>
                 </div>
             </PageDivcontainer>
