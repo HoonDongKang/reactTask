b274d9237bc1acfb25f0815d07bada7293b5f30f
Donghoon Kang <d159123@naver.com>
2022-07-21 23:20:13 +0900

    tets

--- src/components/Verifyemail.tsx
+++ src/components/Verifyemail.tsx
@@ -1,7 +1,21 @@
-const Verifyemail = () => (
-    <>
-        <p>로그인이 되었다.</p>
-    </>
-)
+import { useState, useEffect } from 'react'
+import { apiInstance } from './../apis/setting'
+const Verifyemail = () => {
+    const [email, setEmail] = useState([])
+
+    useEffect(() => {
+        const getRequest = async () => {
+            const response = await apiInstance.put('/mail')
+            setEmail(response.data)
+            console.log(response)
+        }
+        getRequest()
+    }, [])
+    return (
+        <>
+            <p>로그인이 되었다.</p>
+        </>
+    )
+}
 
 export default Verifyemail
