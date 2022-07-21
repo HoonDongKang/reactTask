1bf9ff44e826e2dc1b4adfdbf320d9b6ec1b2b1a
Donghoon Kang <d159123@naver.com>
2022-07-21 23:50:04 +0900

    test

--- src/components/Verifyemail.tsx
+++ src/components/Verifyemail.tsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react'
 import { apiInstance } from './../apis/setting'
 const Verifyemail = () => {
     const [email, setEmail] = useState([])
-
+    const emailtest = 'd159123@naver.com'
     useEffect(() => {
         const getRequest = async () => {
-            const response = await apiInstance.put('/mail')
+            const response = await apiInstance.get(`/mail/${emailtest}`)
             setEmail(response.data)
             console.log(response)
         }
