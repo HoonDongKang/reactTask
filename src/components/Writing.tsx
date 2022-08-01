65fa84ee72664e12785259f3e53c694b3caa194a
Donghoon Kang <d159123@naver.com>
2022-08-01 21:48:38 +0900

    pageinput

--- src/components/Writing.tsx
+++ src/components/Writing.tsx
@@ -1,8 +1,31 @@
+import { useState } from 'react'
 const Writing = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const titleHandler = (e) => {
+        setTitle(e.target.value)
+    }
+    const contentHandler = (e) => {
+        setContent(e.target.value)
+    }
+
+    const submit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>
-            <p>hello</p>
-        </div>
+        <form onSubmit={submit}>
+            <label htmlFor='title'> 제목 : </label>
+            <input
+                id='title'
+                type='text'
+                value={title}
+                onChange={titleHandler}
+            />
+            <label htmlFor='content'> 내용 : </label>
+            <input id='content' type='text' />
+            <input type='submit' value={content} onChange={contentHandler} />
+        </form>
     )
 }
 
