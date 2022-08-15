5a8031fecb9f8eac1fa5d8a60345f70efca8d632
HoonDongKang <d159123@naver.com>
2022-08-15 21:59:16 +0900

    kakao

--- src/components/KaKao/KaKaojs.tsx
+++ src/components/KaKao/KaKaojs.tsx
@@ -11,6 +11,40 @@ const KaKaojs = () => {
         })
     }
     //sdk logout
+    const kakao_quit = () => {
+        window.Kakao.API.request({
+            url: '/v1/user/unlink',
+            success: function (res) {
+                alert('success: ' + JSON.stringify(res))
+            },
+            fail: function (err) {
+                alert('fail: ' + JSON.stringify(err))
+            },
+        })
+    }
+
+    const kakao_info = () => {
+        window.Kakao.Auth.createLoginButton({
+            container: '#kakao-login-btn',
+            success: function (authObj) {
+                window.Kakao.API.request({
+                    url: '/v2/user/me',
+                    success: function (res) {
+                        alert(JSON.stringify(res))
+                    },
+                    fail: function (error) {
+                        alert(
+                            'login success, but failed to request user information: ' +
+                                JSON.stringify(error)
+                        )
+                    },
+                })
+            },
+            fail: function (err) {
+                alert('failed to login: ' + JSON.stringify(err))
+            },
+        })
+    }
     const kakao_logout = () => {
         window.Kakao.API.request({
             url: '/v1/user/unlink',
@@ -25,7 +59,11 @@ const KaKaojs = () => {
     return (
         <div>
             <button onClick={kakao_login}>카톡 로그인</button>
-            <button onClick={kakao_logout}>카톡 연결끊기</button>
+            <button onClick={kakao_quit}>카톡 연결끊기</button>
+            <a id='kakao-login-btn' onClick={kakao_info}>
+                카톡 프로필
+            </a>
+            <button onClick={kakao_logout}>로그아웃</button>
         </div>
     )
 }
