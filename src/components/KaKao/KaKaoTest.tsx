009ce165cd55356b95438a2b58cc2a91eac985ef
HoonDongKang <d159123@naver.com>
2022-08-24 13:33:37 +0900

    kakao

--- src/components/KaKao/KaKaoTest.tsx
+++ src/components/KaKao/KaKaoTest.tsx
@@ -11,6 +11,8 @@ const KaKaoTest = () => {
     const REDIRECT_URI = `http://localhost:3000/kakaotest`
     const [accessToken, setAccessToken] = useState('')
     const [profile, setProfile] = useState({})
+    const [id_token, setId_token] = useState('')
+    const [token_info, setToken_info] = useState({})
     const kakaoLogin = async () => {
         try {
             const code = new URL(window.location.href).searchParams.get('code')
@@ -24,7 +26,26 @@ const KaKaoTest = () => {
                 }
             )
             setAccessToken(response.data.access_token)
+            setId_token(response.data.id_token)
             console.log('in async:', accessToken)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    const giveId_token = async () => {
+        try {
+            const tokenInfo = await axios.post(
+                'https://kauth.kakao.com/oauth/tokeninfo',
+                { id_token: id_token },
+                {
+                    headers: {
+                        'Content-type':
+                            'application/x-www-form-urlencoded;charset=utf-8',
+                    },
+                }
+            )
+            setToken_info(tokenInfo)
+            console.log(`in async tokenInfo: ${tokenInfo}`)
         } catch (e) {
             console.log(e)
         }
@@ -51,14 +72,16 @@ const KaKaoTest = () => {
         kakaoLogin()
         console.log(`useeffect:${accessToken}`)
         getKaKaoProfile() //cors block..
+        giveId_token()
     }, [accessToken])
+    console.log(`out async token_info:${token_info}`)
     console.log('out async:', accessToken)
     console.log('getinfo:', profile)
 
     return (
         <div>
             <p>AccessToken:{accessToken}</p>
-            <p>RefreshToken:{accessToken}</p>
+            <p>id token:{id_token}</p>
         </div>
     )
 }
